6bac88066416d8d3acd47495b3910ca06159caae
yanqui92 <andres.yk@live.com>
2015-06-13 17:33:27 -0300

    Nuevo proyecto

---
+++ pronto02/pronto02/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(pronto02.Startup))]
+namespace pronto02
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
