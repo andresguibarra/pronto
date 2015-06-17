44ca62301d5e13b8b4b0db69d09346fa3aaf5601
federicocasabona <federicocasabona@hotmail.com>
2015-06-16 22:03:36 -0300

    Eliminado Proyecto de sobra

--- pronto/pronto/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="pronto.Global" Language="C#" %>
--- pronto/pronto/prueba.js
+++
@@ -1,8 +0,0 @@
-﻿function button2_OnCLick() {
-    var l1 = document.getElementById('Label1');
-    var l2 = document.getElementById('Label2');
-    var l3 = document.getElementById('Label3');
-    var l4 = document.getElementById('Label4');
-    l4.textContent = l1.textContent + l2.textContent + l3.textContent;
-    return false;
-}
