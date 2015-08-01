dfde1630e114983d74798be1740c892e643006a8
nicofgomez <gomez.nicoo@gmail.com>
2015-08-01 14:34:27 -0300

    Agregada la página de venta sin diseño

---
+++ pronto02/pronto02/Linea_Venta.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace pronto02
+{
+    public class Linea_Venta
+    {
+        #region ATRIBUTOS
+        private String nombre_producto;
+        private int unidades;
+        private float precio;
+        private float subtotal;
+        #endregion
+
+        #region METODOS
+        public Linea_Venta(string np, int u, float p)
+        {
+            this.nombre_producto = np;
+            this.unidades = u;
+            this.precio = p;
+            this.subtotal = u * p;
+        }
+        #endregion
+
+        #region PROPIEDADES
+        public String Nombre_Producto
+        {
+            get
+            {
+                return this.nombre_producto;
+            }
+            set
+            {
+                this.nombre_producto = value;
+            }
+        }
+
+        public int Unidades
+        {
+            get
+            {
+                return this.unidades;
+            }
+            set
+            {
+                this.unidades = value;
+                this.subtotal = unidades * precio;
+            }
+        }
+
+        public float Precio
+        {
+            get
+            {
+                return this.precio;
+            }
+            set
+            {
+                this.precio = value;
+                this.subtotal = unidades * precio;
+            }
+        }
+
+        public float Subtotal
+        {
+            get
+            {
+                return this.subtotal;
+            }               
+        }
+        #endregion
+    }
+}
---
+++ pronto02/pronto02/Venta.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pronto02
+{
+    public partial class Venta : System.Web.UI.Page
+    {
+        PRONTODBEntities db = new PRONTODBEntities();
+        public List<Linea_Venta> listaVentas
+        {
+            get
+            {
+               if(HttpContext.Current.Session["listaVentas"] == null)
+               {
+                   HttpContext.Current.Session["listaVentas"] = new List<Linea_Venta>();
+               }
+               return (List<Linea_Venta>)HttpContext.Current.Session["listaVentas"];
+            }
+            set
+            {
+                HttpContext.Current.Session["listaVentas"] = value;
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataSource = listaVentas;
+            GridView1.DataBind();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            int unidades;
+            float precio;
+            if (int.TryParse(txtUnidades.Text, out unidades) && float.TryParse(txtPrecio.Text.Replace(',','.'), out precio))
+            {
+                Linea_Venta lv = new Linea_Venta(nombre, unidades, precio);
+                listaVentas.Add(lv);
+            }
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+                listaVentas.RemoveAt(e.RowIndex);
+                Response.Redirect(Request.Url.AbsoluteUri);
+        }
+
+        protected void btBuscar_Click(object sender, EventArgs e)
+        {
+            var consulta = from p in db.PRODUCTO
+                           where p.cod_barras == txtCodigoBarras.Text
+                           select p;
+            if (consulta.ToList().Count > 0)
+            {
+                PRODUCTO pr = consulta.First();
+                txtNombre.Text = pr.Nombre;
+                txtPrecio.Text = pr.Precio_venta.ToString();
+                if(String.IsNullOrEmpty(txtUnidades.Text))
+                    txtUnidades.Text = "1";
+                Button1_Click(this, e);
+            }
+        }
+    }
+}
