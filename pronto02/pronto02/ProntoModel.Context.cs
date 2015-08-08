b51072b2be34f46829da988cc1c4c30361f87685
federicocasabona <federicocasabona@hotmail.com>
2015-08-08 16:59:27 -0300

    Ventas

--- pronto02/pronto02/ProntoModel.Context.cs
+++ pronto02/pronto02/ProntoModel.Context.cs
@@ -30,5 +30,6 @@ namespace pronto02
         public virtual DbSet<Vn_Categoria> Vn_Categoria { get; set; }
         public virtual DbSet<Proveedor> Proveedor { get; set; }
         public virtual DbSet<Contacto> Contacto { get; set; }
+        public virtual DbSet<VENTAS> VENTAS { get; set; }
     }
 }
